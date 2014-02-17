9717d50c08297b0505537f7a8a57ffdb861085b0
patrick brisbin <pbrisbin@gmail.com>
2014-02-17 15:55:50 -0500

    Update Campfire integration
    * Assume some coverage specific data is available in payload
    * Extend CoverageHelper
    * Update message contents

--- test/campfire_test.rb
+++ test/campfire_test.rb
@@ -7,15 +7,39 @@ class TestCampfire < CC::Service::TestCase
     end
   end
 
-  def test_coverage_change
+  def test_coverage_improved
+    expected_message = "[Code Climate][Rails] :sunny: Test coverage has improved to 90.2% (+10.2%). (http://codeclimate.com/rails/compare)"
     @stubs.post '/room/123/speak.json' do |env|
       body = JSON.parse(env[:body])
-      assert_equal "Coverage: 80.0", body["message"]["body"]
+      assert_equal expected_message, body["message"]["body"]
       [200, {}, '']
     end
 
     receive(CC::Service::Campfire, :coverage,
       { token: "token", subdomain: "sub", room_id: "123" },
-      { coverage: 80.0 })
+      {
+        repo_name: "Rails",
+        covered_percent: 90.2,
+        previous_covered_percent: 80.0,
+        details_url: "http://codeclimate.com/rails/compare"
+      })
+  end
+
+  def test_coverage_declined
+    expected_message = "[Code Climate][jQuery] :umbrella: Test coverage has declined to 88.6% (-6.0%). (http://codeclimate.com/rails/compare)"
+    @stubs.post '/room/123/speak.json' do |env|
+      body = JSON.parse(env[:body])
+      assert_equal expected_message, body["message"]["body"]
+      [200, {}, '']
+    end
+
+    receive(CC::Service::Campfire, :coverage,
+      { token: "token", subdomain: "sub", room_id: "123" },
+      {
+        repo_name: "jQuery",
+        covered_percent: 88.6,
+        previous_covered_percent: 94.6,
+        details_url: "http://codeclimate.com/rails/compare"
+      })
   end
 end
