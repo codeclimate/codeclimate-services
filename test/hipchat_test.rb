b4c7772761d8b48002b26e13e7ab3d05bd4524be
patrick brisbin <pbrisbin@gmail.com>
2014-02-19 12:14:18 -0500

    Introduce fixtures, QualityHelper fixes
    * QualityHelper#improved? was backwards
    * QualityHelper#with_article was wrong

--- test/hipchat_test.rb
+++ test/hipchat_test.rb
@@ -11,47 +11,32 @@ class TestHipChat < CC::Service::TestCase
   end
 
   def test_coverage_improved
-    assert_hipchat_receives(:coverage, "green", {
-      repo_name: "Rails",
-      covered_percent: 90.2,
-      previous_covered_percent: 80.0,
-      covered_percent_delta: 10.2,
-      details_url: "https://codeclimate.com/repos/1/feed",
-      compare_url: "https://codeclimate.com/repos/1/compare"
-    }, [
-      "[Rails]",
+    e = event(:coverage, to: 90.2, from: 80)
+
+    assert_hipchat_receives(:coverage, "green", e, [
+      "[Example]",
       "<a href=\"https://codeclimate.com/repos/1/feed\">Test coverage</a>",
       "has improved to 90.2% (+10.2%)",
       "(<a href=\"https://codeclimate.com/repos/1/compare\">Compare</a>)"
     ].join(" "))
   end
 
-  def test_coverage_declined_without_compare_url
-    assert_hipchat_receives(:coverage, "red", {
-      repo_name: "Rails",
-      covered_percent: 80.0,
-      previous_covered_percent: 86.2,
-      covered_percent_delta: -6.2,
-      details_url: "https://codeclimate.com/repos/1/feed",
-    }, [
-      "[Rails]",
+  def test_coverage_declined
+    e = event(:coverage, to: 88.6, from: 94.6)
+
+    assert_hipchat_receives(:coverage, "red", e, [
+      "[Example]",
       "<a href=\"https://codeclimate.com/repos/1/feed\">Test coverage</a>",
-      "has declined to 80.0% (-6.2%)"
+      "has declined to 88.6% (-6.0%)",
+      "(<a href=\"https://codeclimate.com/repos/1/compare\">Compare</a>)"
     ].join(" "))
   end
 
   def test_quality_improved
-    assert_hipchat_receives(:quality, "green", {
-      repo_name: "Rails",
-      constant_name: "User",
-      rating: "A",
-      previous_rating: "B",
-      remediation_cost: 50,
-      previous_remediation_cost: 25,
-      details_url: "https://codeclimate.com/repos/1/feed",
-      compare_url: "https://codeclimate.com/repos/1/compare"
-    }, [
-      "[Rails]",
+    e = event(:quality, to: "A", from: "B")
+
+    assert_hipchat_receives(:quality, "green", e, [
+      "[Example]",
       "<a href=\"https://codeclimate.com/repos/1/feed\">User</a>",
       "has improved from a B to an A",
       "(<a href=\"https://codeclimate.com/repos/1/compare\">Compare</a>)"
@@ -59,63 +44,53 @@ class TestHipChat < CC::Service::TestCase
   end
 
   def test_quality_declined_without_compare_url
-    assert_hipchat_receives(:quality, "red", {
-      repo_name: "Rails",
-      constant_name: "User",
-      rating: "D",
-      previous_rating: "C",
-      remediation_cost: 25,
-      previous_remediation_cost: 50,
-      details_url: "https://codeclimate.com/repos/1/feed",
-    }, [
-      "[Rails]",
+    e = event(:quality, to: "D", from: "C")
+
+    assert_hipchat_receives(:quality, "red", e, [
+      "[Example]",
       "<a href=\"https://codeclimate.com/repos/1/feed\">User</a>",
       "has declined from a C to a D",
+      "(<a href=\"https://codeclimate.com/repos/1/compare\">Compare</a>)"
     ].join(" "))
   end
 
   def test_single_vulnerability
-    assert_hipchat_receives(:vulnerability, "red", {
-      repo_name: "Rails",
-      vulnerabilities: [{ "warning_type" => "critical" }],
-      details_url: "https://codeclimate.com/repos/1/vulnerabilities"
-    }, [
-      "[Rails]",
-      "New <a href=\"https://codeclimate.com/repos/1/vulnerabilities\">critical</a>",
+    e = event(:vulnerability, vulnerabilities: [
+      { "warning_type" => "critical" }
+    ])
+
+    assert_hipchat_receives(:vulnerability, "red", e, [
+      "[Example]",
+      "New <a href=\"https://codeclimate.com/repos/1/feed\">critical</a>",
       "issue found.",
     ].join(" "))
   end
 
   def test_single_vulnerability_with_location
-    assert_hipchat_receives(:vulnerability, "red", {
-      repo_name: "Rails",
-      vulnerabilities: [{
-        "warning_type" => "critical",
-        "location" => "app/user.rb line 120"
-      }],
-      details_url: "https://codeclimate.com/repos/1/vulnerabilities"
-    }, [
-      "[Rails]",
-      "New <a href=\"https://codeclimate.com/repos/1/vulnerabilities\">critical</a>",
+    e = event(:vulnerability, vulnerabilities: [{
+      "warning_type" => "critical",
+      "location" => "app/user.rb line 120"
+    }])
+
+    assert_hipchat_receives(:vulnerability, "red", e, [
+      "[Example]",
+      "New <a href=\"https://codeclimate.com/repos/1/feed\">critical</a>",
       "issue found in app/user.rb line 120.",
     ].join(" "))
   end
 
   def test_multiple_vulnerabilities
-    assert_hipchat_receives(:vulnerability, "red", {
-      repo_name: "Rails",
-      warning_type: "critical",
-      vulnerabilities: [{
-        "warning_type" => "unused",
-        "location" => "unused"
-      }, {
-        "warning_type" => "unused",
-        "location" => "unused"
-      }],
-      details_url: "https://codeclimate.com/repos/1/vulnerabilities"
-    }, [
-      "[Rails]",
-      "2 new <a href=\"https://codeclimate.com/repos/1/vulnerabilities\">critical</a>",
+    e = event(:vulnerability, warning_type: "critical", vulnerabilities: [{
+      "warning_type" => "unused",
+      "location" => "unused"
+    }, {
+      "warning_type" => "unused",
+      "location" => "unused"
+    }])
+
+    assert_hipchat_receives(:vulnerability, "red", e, [
+      "[Example]",
+      "2 new <a href=\"https://codeclimate.com/repos/1/feed\">critical</a>",
       "issues found.",
     ].join(" "))
   end
