aa0dae06f352758ff42dc24f79b9e1861ec0ee44
GordonDiggs <gordon@gordondiggs.com>
2015-09-22 12:10:56 -0400

    Add StashPullRequest service
    This will allow us to update PRs on Stash instances.
    
    Documentation for the Stash build status API:
    https://developer.atlassian.com/stash/docs/latest/how-tos/updating-build-status-for-commits.html

---
+++ lib/cc/services/stash_pull_requests.rb
@@ -0,0 +1,98 @@
+require "base64"
+require "cc/presenters/pull_requests_presenter"
+
+class CC::Service::StashPullRequests < CC::Service
+  class Config < CC::Service::Config
+    attribute :domain, String,
+      description: "Your Stash host domain (e.g. yourstash.com:PORT, please exclude https://)"
+    attribute :username, String
+    attribute :password, Password
+
+    validates :domain, presence: true
+    validates :username, presence: true
+    validates :password, presence: true
+  end
+
+  self.title = "Stash Pull Requests"
+  self.description = "Update pull requests on Stash"
+
+  STASH_STATES = {
+    "error" => "FAILED",
+    "failure" => "FAILED",
+    "pending" => "INPROGRESS",
+    "skipped" => "SUCCESSFUL",
+    "success" => "SUCCESSFUL",
+  }.freeze
+
+  def receive_test
+    setup_http
+
+    service_get(test_url)
+
+    { ok: true, message: "Test succeeded" }
+  rescue HTTPError => e
+    { ok: false, message: e.message }
+  end
+
+  def receive_pull_request
+    setup_http
+
+    params = {
+      description: description,
+      key: "Code Climate",
+      name: "Code Climate",
+      state: state,
+      url:  @payload["details_url"],
+    }
+    service_post(url, params.to_json)
+  end
+
+  private
+
+  def test_url
+    "http://#{config.domain}/rest/api/1.0/users"
+  end
+
+  def url
+    "http://#{config.domain}/rest/build-status/1.0/commits/#{commit_sha}"
+  end
+
+  def commit_sha
+    @payload.fetch("commit_sha")
+  end
+
+  def description
+    return @payload["message"] if @payload["message"]
+
+    case @payload["state"]
+    when "pending"
+      "Code Climate is analyzing this code."
+    when "success", "failure"
+      presenter.success_message
+    when "skipped"
+      "Code Climate has skipped analysis of this commit."
+    when "error"
+      "Code Climate encountered an error attempting to analyze this pull request."
+    end
+  end
+
+  def state
+    STASH_STATES[@payload["state"]]
+  end
+
+  def setup_http
+    http.headers["Content-Type"]  = "application/json"
+    http.headers["Authorization"] = "Basic #{auth_token}"
+    http.headers["User-Agent"]    = "Code Climate"
+  end
+
+  # Following Basic Auth headers here:
+  # https://developer.atlassian.com/stash/docs/latest/how-tos/example-basic-authentication.html
+  def auth_token
+    Base64.encode64("#{config.username}:#{config.password}")
+  end
+
+  def presenter
+    CC::Service::PullRequestsPresenter.new(@payload)
+  end
+end
