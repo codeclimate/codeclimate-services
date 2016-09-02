1ad7a6ad754a0fbd8a7b33df32e199293b6411bd
Will Fleming <will@flemi.ng>
2016-09-02 14:55:30 -0400

    turn off meaningless option?

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -31,13 +31,6 @@ RSpec.configure do |config|
     mocks.verify_partial_doubles = true
   end
 
-  # This option will default to `:apply_to_host_groups` in RSpec 4 (and will
-  # have no way to turn it off -- the option exists only for backwards
-  # compatibility in RSpec 3). It causes shared context metadata to be
-  # inherited by the metadata hash of host groups and examples, rather than
-  # triggering implicit auto-inclusion in groups with matching metadata.
-  config.shared_context_metadata_behavior = :apply_to_host_groups
-
   # This setting enables warnings. It's recommended, but in some cases may
   # be too noisy due to issues in dependencies.
   config.warnings = true
