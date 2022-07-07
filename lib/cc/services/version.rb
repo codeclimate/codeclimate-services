b6cfb4f5dc10c3c4c7103679a2bda58306084b3d
Federico Moyá <federicomoyamartin@gmail.com>
2022-07-07 13:50:04 -0300

    Fix version path

--- lib/cc/services/version.rb
+++ lib/cc/services/version.rb
@@ -1,8 +1,9 @@
 module CC
   module Services
     def version
-      path = File.expand_path("../../../VERSION", __FILE__)
-      @version ||= File.read(path)
+      path = File.expand_path("../../../../VERSION", __FILE__)
+      @version ||= File.read(path).strip
     end
+    module_function :version
   end
 end
